7ede6960edbe8837d8faf3ef8a9465c466899577
Patryk Duński <patryk.dunski@kende.pl>
2018-08-16 10:19:33 +0200

    Reduces timeout to 17

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -57,7 +57,7 @@ export class StatusComponent {
   startTimer() {
     this.counter$ = interval(1000).pipe(
       map(() => {
-        return this.ongoing ? Math.floor((new Date(this.ongoing.startDate).getTime() + (30 * 60000) - new Date().getTime()) / 1000) : 0;
+        return this.ongoing ? Math.floor((new Date(this.ongoing.startDate).getTime() + (17 * 60000) - new Date().getTime()) / 1000) : 0;
       })
     );
     this.counter$.subscribe((x) => {
@@ -71,6 +71,6 @@ export class StatusComponent {
   }
 
   getETA(game)  {
-    return this.ongoing ? new Date(this.ongoing.startDate).getTime() + (this.games.size * 30 * 60000) : new Date();
+    return this.ongoing ? new Date(this.ongoing.startDate).getTime() + (this.games.size * 17 * 60000) : new Date();
   }
 }
